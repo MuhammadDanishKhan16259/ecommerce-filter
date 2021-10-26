0b65730648846844c782ca26a1653c0ba786b99c
Danish khan <muhammaddanishkhan1999@gmail.com>
2021-10-26 20:32:47 +0500

    All done

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>Danish @2020</p>
+      <div className="social">
+        <i className="fa fa-facebook"></i>
+        <i className="fa fa-instagram"></i>
+      </div>
+    </div>
+  );
+}
